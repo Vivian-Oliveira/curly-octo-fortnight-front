c626e449e68b4c47e52a05e2ca3532b01510e85e
Vivian-Oliveira <costa.vivs@gmail.com>
2022-01-05 16:26:18 +0000

    feat: create hook useContract

---
+++ app/src/hooks/useContract/index.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import abi from "../../utils/MediaShare.json";
+import { Contract } from "web3-eth-contract";
+
+declare let window: any;
+
+export function useContract() {
+  const [contract, setContract] = useState<Contract | null>(null);
+  const contractAddress = "0x2D9F95DBA5e3Ddd529F56CbE72836D51Fc8AB289";
+  const contractABI = abi.abi;
+
+  async function fetchContract() {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract: any = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        setContract(wavePortalContract);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchContract();
+  }, []);
+
+  return {
+    contract,
+    fetchContract,
+  };
+}
