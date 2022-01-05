2bd76dbdff1fcce4640e372e8568b1921dc18d38
Clara Mendes <claraplanejamento@gmail.com>
2022-01-05 15:58:46 -0300

    :lipstick: adds favorite button

--- app/src/components/MediaCard/index.tsx
+++ app/src/components/MediaCard/index.tsx
@@ -1,8 +1,12 @@
+import { CardActions, IconButton } from "@mui/material";
 import CardActionArea from "@mui/material/CardActionArea";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import { getUrl } from "../../lib/UrlConvert";
 import { MediaCardContainer, MediaCardContent } from "./styles";
+import { useState } from "react";
+import { greyColor, neutralColor } from "../../styles/colors";
 
 export type Props = {
   src: any;
@@ -12,13 +16,19 @@ export type Props = {
 
 const MediaCard = (props: Props) => {
   const { src, title, onClick } = props;
+  const [favorited, setFavorited] = useState(false);
   const url = getUrl(src);
+
+  function getColor() {
+    return favorited ? neutralColor : greyColor;
+  }
+
   return (
-    <MediaCardContainer onClick={props.onClick}>
+    <MediaCardContainer onClick={onClick}>
       <CardActionArea>
         <CardMedia
           component={"iframe"}
-          width="560px" 
+          width="560px"
           height="315px"
           title={title}
           src={url}
@@ -27,6 +37,15 @@ const MediaCard = (props: Props) => {
         <MediaCardContent>
           <Typography align={"center"}>{props.title.toUpperCase()}</Typography>
         </MediaCardContent>
+
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon
+              sx={{ color: getColor() }}
+              onClick={() => setFavorited(!favorited)}
+            />
+          </IconButton>
+        </CardActions>
       </CardActionArea>
     </MediaCardContainer>
   );
