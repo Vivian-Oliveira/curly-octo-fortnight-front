b07beceee38dd2eaf46a4565e85baf15c5d45549
Vivian-Oliveira <costa.vivs@gmail.com>
2022-01-05 16:37:41 +0000

    feat: create hook useWallet

---
+++ app/src/hooks/useWallet/index.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+declare let window: any;
+
+export function useWallet() {
+  const [account, setAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return {
+    account,
+    checkIfWalletIsConnected,
+    connectWallet,
+  };
+}
