3308d84a9c82d89b7a4012023ddd890b2a3ca1a5
Vivian-Oliveira <costa.vivs@gmail.com>
2022-01-05 16:46:26 +0000

    feat: add wallet connection

--- app/src/pages/MainPage/index.tsx
+++ app/src/pages/MainPage/index.tsx
@@ -1,9 +1,22 @@
 import MediaCard from "../../components/MediaCard";
 import { Container } from "./styles";
+import { useWallet } from "../../hooks/useWallet/index";
+import { useEffect } from "react";
 
 function MainPage() {
+  const { account, connectWallet, checkIfWalletIsConnected } = useWallet();
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <Container>
+      {!account && (
+        <button className="waveButton" onClick={connectWallet}>
+          Connect Wallet
+        </button>
+      )}
 
       <MediaCard
         title="teste"
